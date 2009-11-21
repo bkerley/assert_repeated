4f18ee539d1ad27ee51a7282af56260b4aa72830
Bryce Kerley <bkerley@brycekerley.net>
2009-11-21 13:59:41 -0800

    simple repetition tests

--- test/test_assert_repeated.rb
+++ test/test_assert_repeated.rb
@@ -1,7 +1,37 @@
 require 'helper'
 
 class TestAssertRepeated < Test::Unit::TestCase
-  def test_something_for_real
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  def test_one_passing
+    assert_nothing_raised do
+      assert_repeatedly_true(1){ true }
+    end
+  end
+
+  def test_one_failure
+    assert_raise Test::Unit::AssertionFailedError do
+      assert_repeatedly_true(1){ false }
+    end
+  end
+
+  def test_a_hundred_passings
+    count = 0
+    assert_repeatedly_true 100 do
+      count = count + 1
+      true
+    end
+
+    assert_equal 100, count
+  end
+
+  def test_first_failure
+    count = 0
+    assert_raise Test::Unit::AssertionFailedError do
+      assert_repeatedly_true 100 do
+        count = count + 1
+        false
+      end
+    end
+
+    assert_equal 1, count
   end
 end
