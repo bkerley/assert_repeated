4d92355f909734066580375a27d6f5797965d143
Bryce Kerley <bkerley@brycekerley.net>
2009-11-21 14:46:31 -0800

    use assert_repeatedly

--- lib/assert_repeated.rb
+++ lib/assert_repeated.rb
@@ -1,23 +1,16 @@
 module AssertRepeated
-  def assert_repeatedly_true(count, message=nil)
-    count.times do
-      result = yield
-      assert(false, build_message(message, "<?> is not true", result)) unless result
-    end
-    # increment the count
-    assert true
+  def assert_repeatedly_true(count, message=nil, &block)
+    assert_repeatedly count, true, message, &block
   end
 
   def assert_repeatedly_false(count, message=nil, &block)
-    assert_repeatedly_true(count, message) do
-      ! block.call
-    end
+    assert_repeatedly count, false, message, &block
   end
 
   def assert_repeatedly(count, matcher=true, message=nil)
     count.times do
       result = yield
-      assert(false, build_message(message, "<?> is not true", result)) unless matcher === result
+      assert_block(build_message(message, "<?> is not ?.", result, matcher)){ false } unless matcher === result
     end
     # increment the count
     assert true
