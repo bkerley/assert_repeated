1341d7d7234cc4448c24381df4dfee64b522d7ee
Bryce Kerley <bkerley@brycekerley.net>
2009-11-21 14:46:20 -0800

    simple message-passing tests

--- test/test_assert_repeated.rb
+++ test/test_assert_repeated.rb
@@ -8,7 +8,7 @@ class TestAssertRepeated < Test::Unit::TestCase
   end
 
   def test_one_failure
-    assert_raise Test::Unit::AssertionFailedError do
+    assert_assertion_failure_raised_with_message "<false> is not true." do
       assert_repeatedly_true(1){ false }
     end
   end
@@ -25,7 +25,7 @@ class TestAssertRepeated < Test::Unit::TestCase
 
   def test_first_failure
     count = 0
-    assert_raise Test::Unit::AssertionFailedError do
+    assert_assertion_failure_raised_with_message "<false> is not true." do
       assert_repeatedly_true 100 do
         count = count + 1
         false
@@ -47,9 +47,25 @@ class TestAssertRepeated < Test::Unit::TestCase
       assert_repeatedly(1, /x/) { 'x' }
     end
 
-    assert_raise Test::Unit::AssertionFailedError do
+    assert_assertion_failure_raised_with_message "<8> is not 7." do
       assert_repeatedly(1, 7) { 8 }
+    end
+
+    assert_assertion_failure_raised_with_message "<\"f\"> is not /x/." do
       assert_repeatedly(1, /x/) { 'f' }
+    end
+  end
+
+  private
+  # bonus secret assertion
+  def assert_assertion_failure_raised_with_message(message)
+    begin
+      yield
+      assert false, "No exception raised"
+    rescue Test::Unit::AssertionFailedError => e
+      assert_equal message, e.message
+    rescue Exception => e
+      assert false, "Unexpected exception <e> raised"
     end
   end
 end
