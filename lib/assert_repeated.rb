a7d5d0f8c644c8d083e6aff1583a136b1404d085
Bryce Kerley <bkerley@brycekerley.net>
2009-11-22 15:57:47 -0600

    use yardoc 0.4 tags

--- lib/assert_repeated.rb
+++ lib/assert_repeated.rb
@@ -1,31 +1,39 @@
+
+# Assertions that run multiple times, used to smoke-test or
+# randomly probe a function with a large domain.
+#
+# Use +assert_repeatedly_true+ or +assert_repeatedly_false+ in most cases,
+# or use +assert_repeatedly+ to build your own.
 module AssertRepeated
-  # Assertions that run multiple times, used to smoke-test or
-  # randomly probe a function with a large domain.
+  # Check that the block returns +true+ +count+ times.
   #
-  # == Usage
-  #
+  # @param [Integer] count number of times to run the block
+  # @param [optional, String] message optional message to return when the assertion fails
+  # @yield block that runs for the assertion; expected to evaluate to +true+
+  # @raise Test::Unit::AssertionFailedError
+  # @example Test encryption and decryption
   #    assert_repeatedly_true(1000) do
   #      expected = rand(100)
   #      actual = decrypt(encrypt(expected))
   #      expected == actual
   #    end
-
-  # Check that the block returns +true+ +count+ times.
-  #
-  # == Arguments
-  # [+count+] number of times to run the block
-  # [+message+] optional message to return when the assertion fails
-  # [+block+] block that runs for the assertion; expected to evaluate to +true+
   def assert_repeatedly_true(count, message=nil, &block)
     assert_repeatedly count, true, message, &block
   end
 
   # Check that the block returns +false+ +count+ times.
   #
-  # == Arguments
-  # [+count+] number of times to run the block
-  # [+message+] optional message to return when the assertion fails
-  # [+block+] block that runs for the assertion; expected to evaluate to +false+
+  # @param [Integer] count number of times to run the block
+  # @param [optional, String] message optional message to return when the assertion fails
+  # @yield block that runs for the assertion; expected to evaluate to +false+
+  # @raise Test::Unit::AssertionFailedError
+  # @example Test cryptographic digest function
+  #    assert_repeatedly_true(1000) do
+  #      expected = rand(0xFF)
+  #      muted = expected ^ rand(0xFE) + 1
+  #      actual = digest(expected)
+  #      actual == digest(muted)
+  #    end
   def assert_repeatedly_false(count, message=nil, &block)
     assert_repeatedly count, false, message, &block
   end
@@ -36,10 +44,15 @@ module AssertRepeated
   #
   #   assert_repeatedly(100, /awesome/) { "tests are awesome" }
   #
-  # == Arguments
-  # [+count+] number of times to run the block
-  # [+matcher+] what to compare against the block's result (using +#===+ )
-  # [+message+] optional message to return when the assertion fails
+  # @param [Integer] count number of times to run the block
+  # @param [Object] matcher what to compare against the block's result (using +#===+ )
+  # @param [optional, String] message optional message to return when the assertion fails
+  # @yield block that runs for the assertion; expected to be threequal to +matcher+
+  # @raise Test::Unit::AssertionFailedError
+  # @example Implementing assert_repeatedly_bananas
+  #    def assert_repeatedly_bananas(count, &block)
+  #      assert_repeatedly count, /ba(na)+$/, "Must be a plantain!", &block
+  #    end
   def assert_repeatedly(count, matcher=true, message=nil)
     count.times do
       result = yield
